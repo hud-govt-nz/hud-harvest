e3d7f0c161c0cab24ef0613585801b50141ae1eb
Keith Ng <keith@point.org.nz>
2023-01-12 23:36:02 +1300

    Re-org taskmaster

--- taskmaster/__init__.py
+++ taskmaster/__init__.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import pandas as pd
 
 ROOT_PATH = pathlib.Path(__file__).parent.absolute()
-LOG_COLS = ["script", "status", "errors", "start", "end", "input_urls", "input_md5s", "output_urls", "output_md5s"]
 STATUSES = {
     "unassigned": "\033[0;35m",
     "success": "\033[0;32m",
@@ -44,9 +43,6 @@ def get_descendents(t):
         out += get_descendents(c)
     return out
 
-def print_divider(name, colour = "\033[1;36m"):
-    print(f"\n{colour}===  {name}  ===\033[0m")
-
 
 #=============#
 #  Run tasks  #
@@ -61,7 +57,8 @@ async def gather_with_concurrency(tasks, max_tasks):
     return res
 
 # Checks task against last_run
-def is_changed(t, k):
+def is_changed(t):
+    k = "input_md5s"
     lr = t.get("last_run")
     if not lr: return True # If there is no record of last run, consider changed
     prev = lr[k]
@@ -72,15 +69,9 @@ def is_changed(t, k):
 # Checks whether a task is ready to run
 def is_ready(t, forced):
     if t["status"] != "unassigned": return False # Already ran/running
-    deps = t.get("parents") or []
-    if any([d["status"] != "success" for d in deps]): return False # Waiting on dependencies
-    for i,o in [("input_urls", "output_urls"), ("input_md5s", "output_md5s")]:
-        t[i] = []
-        for d in t.get("parents") or []:
-            if type(d[o]) is list: t[i] += d[o]
-            else: t[i].append(d[o])
-    # Check input hashes
-    if forced or is_changed(t, "input_md5s"):
+    for d in t.get("parents"):
+        if d["status"] != "success": return False # Waiting on dependencies
+    if forced or is_changed(t):
         return True
     else:
         for c in get_descendents(t) + [t]:
@@ -127,11 +118,22 @@ def dump_task(args, stdout, stderr):
     safe_args = [re.sub(r"([\s])", r"\\\1", a) for a in args]
     print(" ".join(safe_args))
 
+def print_divider(name, colour = "\033[1;36m"):
+    print(f"\n{colour}===  {name}  ===\033[0m")
+
 
 #===============#
 #  Report/logs  #
 #===============#
-# Recursive branch print used by update_state()
+def print_tree(tasks, clear = True):
+    top_level = [t for t in tasks if not t["parents"]]
+    if clear:
+        for i in range(0, len(tasks) + len(top_level)):
+            print('\033[1A', end='\x1b[2K')
+    for t in top_level:
+        print_branch(t, is_top = True)
+
+# Recursive branch print used by print_tree()
 def print_branch(t, prefix = "", is_top = False, is_last = False):
     script = t["script"]
     status = t["status"]
@@ -164,34 +166,50 @@ def get_last_run(s):
 #=======#
 #  Run  #
 #=======#
+# The top-level definition is in jobs.json, which is a list of jobs
+# Each job has a name, job-wide parameters, and a list of steps
+# Each step is a task, or a list of tasks
+# Each task is one R/Python script
 class Taskmaster:
-    def __init__(self, jobs, proj_name = "bot", root_path = ROOT_PATH):
-        self.name = proj_name
-        self.path = root_path
-        self.script_path = pathlib.Path(f"{self.path}/scripts")
-        self.output_path = pathlib.Path(f"{self.path}/outputs")
+    def __init__(self, jobs, run_name = "test_run", script_path = "modules"):
         self.jobs = jobs
-        self.tasks = self.calc_tasks(jobs)
+        self.run_name = run_name
+        self.script_path = pathlib.Path(script_path)
 
-    def update_state(self, clear = True):
-        tasks = self.tasks
-        top_level = [t for t in tasks if not t["parents"]]
-        if clear:
-            for i in range(0, len(tasks) + len(top_level)):
-                print('\033[1A', end='\x1b[2K')
-        for t in top_level:
-            print_branch(t, is_top = True)
+    @staticmethod
+    def parse_args():
+        parser = argparse.ArgumentParser(description = "Runs jobs defined in controller.")
+        parser.add_argument("--max-tasks",
+                            type = int,
+                            default = 8,
+                            help = "Number of tasks to run simultaneously (defaults to 8)")
+        parser.add_argument("--forced",
+                            action = "store_const",
+                            const = True,
+                            help = "Run even if input hashes are unchanged.")
+        parser.add_argument("--debug",
+                            action = "store_const",
+                            const = True,
+                            help = "Halt and catch fire on errors.")
+        parser.add_argument("--only-run",
+                            metavar = "O",
+                            nargs = "+",
+                            help = "Specific scripts to run. All required dependencies will run with it. "
+                                   "If blank, all scripts will run (this is usually what you want).")
+        return vars(parser.parse_args())
 
-    # Create and verify a single task
+    def log(self, message, level = "info"):
+        logging.info(message)
+
+    # Verifies a single task and compiles everything it needs to run
     def make_task(self, job, script):
         name = job.get("name") or "Unnamed"
         fn = self.script_path.joinpath(script)
         if not os.path.isfile(fn):
-            raise Exception(f"Script '{script}' not found (requested by job '{name}')!")
+            raise Exception(f"Script '{fn}' not found (requested by job '{name}')!")
         return {
-            "name": name,
             "script": script,
-            "subfolder": job.get("subfolder"),
+            "job": job,
             "status": "unassigned", # All tasks start out unassigned
             "last_run": get_last_run(script), # Fetch last run from log
             "parents": [],
@@ -199,22 +217,27 @@ class Taskmaster:
         }
 
     # Break jobs down into interdependent tasks
-    def calc_tasks(self, jobs):
+    def list_tasks(self, jobs, only_run = None):
         tasks = {}
-        for j in jobs:
+        for job in jobs:
             curr = []
-            for step in j["tasks"]:
+            for step in job["steps"]:
                 prev = curr
                 curr = []
                 if type(step) is not list: step = [step]
-                for s in step:
-                    tasks[s] = tasks.get(s) or self.make_task(j, s)
-                    curr.append(tasks[s])
+                for script in step:
+                    task = tasks.get(script) or self.make_task(job, script)
+                    tasks[script] = task
+                    curr.append(task)
                 # Link dependencies
-                for t in curr: t["parents"] = prev
+                for t in curr: t["parents"] = get_uniq(t["parents"] + prev)
                 for t in prev: t["children"] = get_uniq(t["children"] + curr)
-        logging.info(f"{len(jobs)} jobs with {len(tasks)} individual tasks loaded.")
-        return list(tasks.values())
+        self.log(f"{len(jobs)} jobs with {len(tasks)} individual tasks loaded.")
+        tasks = list(tasks.values())
+        if only_run:
+            tasks = self.filter_tasks(tasks, only_run)
+            self.log(f"Limiting to {len(tasks)} tasks related to {only_run}.")
+        return tasks
 
     # Reduce task list to a selection and its ancestors
     def filter_tasks(self, tasks, only_run):
@@ -229,32 +252,30 @@ class Taskmaster:
         return selected
 
     # Prepare arguments for subprocesses
-    def prep_args(self, t, batch_name):
+    def prep_args(self, t):
         name, ext = t["script"].lower().split(".")
+        script_fn = str(self.script_path.joinpath(t["script"]))
         if ext == "py":
-            args = "pipenv run python3".split(" ")
+            args = ["pipenv", "run", "python", script_fn]
         elif ext == "r":
-            args = "Rscript".split(" ")
+            args = ["Rscript", script_fn]
         else:
             raise Exception(f"I don't know how to run files with '.{ext}' extensions!")
-        script = self.script_path
-        output = self.output_path.joinpath(batch_name)
-        if t.get("subfolder"): output = output.joinpath(t["subfolder"])
-        args += [
-            str(script.joinpath(t["script"])),
-            str(output.joinpath(name))
-        ]
-        args += t.get("input_urls") or []
+        args += self.prep_extra_args(t)
         return args
 
+    # Replace this with a custom function if you want to pass extra arguments
+    def prep_extra_args(self, t):
+        return []
+
     # Run a single task
-    async def run_task(self, t, batch_name, debug = False):
+    async def run_task(self, t, debug = False):
         # Initialise task
         t["start"] = datetime.now()
         t["status"] = "running"
         # Run subprocess
         pipe = asyncio.subprocess.PIPE
-        args = self.prep_args(t, batch_name)
+        args = self.prep_args(t)
         proc = await asyncio.create_subprocess_exec(*args, stdout = pipe, stderr = pipe)
         stdout, stderr = [s.decode().strip() for s in await proc.communicate()]
         # Process status/output
@@ -262,53 +283,41 @@ class Taskmaster:
             if proc.returncode != 0:
                 raise Exception(f"{t['script']} failed!")
             r = read_result(stdout)
-            t.update(r)
-            hash_output(t)
+            t.update(r) # All outputs are saved to the task
+            print_tree(self.tasks)
         except:
             t["status"] = "failed"
             t["errors"] = stderr.split("\n")
-            self.update_state()
+            print_tree(self.tasks)
             if debug:
                 dump_task(args, stdout, stderr)
                 print_divider("Traceback")
                 raise
         # Checkout task
         t["end"] = datetime.now()
-        self.update_state()
         write_log(t)
         return t
 
     # Runs all tasks until no ready tasks are available
     def run(self, max_tasks = 8, forced = False, debug = False, only_run = None):
-        batch_name = f"{self.name}-{datetime.now():%Y%m%d}"
-        if only_run:
-            self.tasks = self.filter_tasks(self.tasks, only_run)
-            logging.info(f"Limiting to {len(self.tasks)} tasks related to {only_run}.")
-        # Create root output folders
-        tasks = self.tasks
-        batch_path = self.output_path.joinpath(batch_name)
-        for p in [self.output_path, batch_path]:
-            if not os.path.exists(p): os.mkdir(p)
-        # Create subfolders
-        for t in tasks:
-            if t.get("subfolder"):
-                p = batch_path.joinpath(t["subfolder"])
-                if not os.path.exists(p): os.mkdir(p)
+        start = datetime.now()
+        tasks = self.tasks = self.list_tasks(self.jobs, only_run)
+        print_tree(tasks, clear = False) # Print once to allocate lines
         try:
-            self.update_state(clear = False) # Print once to allocate lines
             while True:
                 ready = [t for t in tasks if is_ready(t, forced)]
                 if ready:
-                    curr_tasks = [self.run_task(t, batch_name, debug) for t in ready]
-                    self.update_state() # Update once all the ready tasks have been assigned
+                    curr_tasks = [self.run_task(t, debug) for t in ready]
+                    print_tree(tasks) # Update once all the ready tasks have been assigned
                     operation = gather_with_concurrency(curr_tasks, max_tasks)
                     res = asyncio.run(operation)
                 else:
+                    # No more tasks ready to run, skip any outstanding tasks
+                    for t in tasks:
+                        if t["status"] == "unassigned":
+                            t["status"] = "skipped"
+                    print_tree(tasks)
+                    self.log(f"\nFinished in {datetime.now() - start}s.")
                     break
-            for t in tasks:
-                if t["status"] == "unassigned":
-                    t["status"] = "incomplete"
-            self.update_state()
-            print("\nDone.")
         except KeyboardInterrupt:
-            logging.info("Aborted.")
+            self.log("Aborted.")
