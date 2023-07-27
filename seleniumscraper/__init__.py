dcfd3906d53b02b31f3030dd7596d7d9321b3a46
Keith Ng <1203875+keithng@users.noreply.github.com>
2023-07-27 13:09:51 +1200

    ChromeDriverManager is no longer required

--- seleniumscraper/__init__.py
+++ seleniumscraper/__init__.py
@@ -4,11 +4,14 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-# Update Chrome on load, so that parallel runs won't try to install on top of each other
+# Start a session to trigger a Chrome update
+# Sometimes we want to do this specifically, so that parallel runs won't try to install on top of each other
 def selenium_update():
-    ChromeDriverManager().install()
+    service = Service()
+    options = Options()
+    driver = WebDriver(service = service, options = options)
+    driver.quit()
 
 # Download using a Selenium browser (i.e. Automated bot browser)
 def selenium_download(src_url, dst_fn, max_wait = 300):
@@ -16,7 +19,7 @@ def selenium_download(src_url, dst_fn, max_wait = 300):
     display.start()
     options = Options()
     options.experimental_options["prefs"] = { "download.default_directory": "/tmp" }
-    service = Service(ChromeDriverManager().install())
+    service = Service()
     driver = WebDriver(service = service, options = options)
     driver.get(src_url)
     tmp_fn = f"/tmp/{src_url.split('?')[0].split('/')[-1]}"
@@ -32,12 +35,12 @@ def selenium_download(src_url, dst_fn, max_wait = 300):
             break
     else:
         raise Exception("Could not download file!")
-    driver.close()
+    driver.quit()
 
 def selenium_get_page(page_url):
     display = Display(visible=0, size=(800, 600))
     display.start()
-    service = Service(ChromeDriverManager().install())
+    service = Service()
     driver = WebDriver(service = service)
     driver.get(page_url)
     page = driver.page_source
