7056629e9aef7dcfab800fe7711fb57444fe48e9
Keith Ng <1203875+keithng@users.noreply.github.com>
2024-12-23 22:17:58 +1300

    Playwright failures need to be caught with timeout

--- playwrightscraper/__init__.py
+++ playwrightscraper/__init__.py
@@ -1,27 +1,34 @@
 import time, random
 from playwright.sync_api import sync_playwright
 from playwright_stealth import stealth_sync
+from playwright._impl import _errors as PlaywrightErrors
 
-# Download using a Selenium browser (i.e. Automated bot browser)
-def playwright_download(file_url, dst_fn):
+
+# Download using Playwright
+# Try all three browsers before declaring failure - sometimes the antibot measures stop one but not others
+def playwright_download(file_url, dst_fn, debug_path = None):
     with sync_playwright() as p:
-        browser_list = [p.firefox, p.webkit, p.chromium]
-        random.shuffle(browser_list)
+        browser_list = [p.firefox, p.webkit, p.chromium] # Firefox is the most reliable for getting past antibot
         for browser_type in browser_list:
             browser = browser_type.launch()
             page = browser.new_page()
             stealth_sync(page)
-            with page.expect_download() as download_info:
-                try: page.goto(file_url) # Direct downloads trigger an error, which is unhelpful
-                except: pass
-            if download_info.is_done() and download_info.value:
+            # Downloads should trigger an error (yes this is a weird way for it to work)
+            try:
+                with page.expect_download(timeout = 5000) as download_info:
+                    res = page.goto(file_url)
+            # If download does not occur, timeout will trigger instead
+            except PlaywrightErrors.TimeoutError:
+                print(f"Got code {res.status} with {browser_type.name}!")
+                if debug_path:
+                    page.screenshot(path = f'{debug_path}/{browser_type.name}-failed.png')
+                browser.close()
+            # If download triggers, save the file
+            except PlaywrightErrors.Error:
                 download = download_info.value
                 download.save_as(dst_fn)
                 browser.close()
-                return(dst_fn)
-            else:
-                browser.close()
-                time.sleep(5)
+                return dst_fn
         else:
             raise Exception("All attempts to download file failed!")
 
@@ -29,8 +36,7 @@ def playwright_download(file_url, dst_fn):
 # Try all three browsers before declaring failure - sometimes the antibot measures stop one but not others
 def playwright_get_page(page_url, debug_path = None):
     with sync_playwright() as p:
-        browser_list = [p.firefox, p.webkit, p.chromium]
-        random.shuffle(browser_list)
+        browser_list = [p.firefox, p.webkit, p.chromium] # Firefox is the most reliable for getting past antibot
         for browser_type in browser_list:
             browser = browser_type.launch()
             page = browser.new_page()
